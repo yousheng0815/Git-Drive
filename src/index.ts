3d1be1168b5b3c53417a4c32185befe2025755c0
You-Sheng Huang <yousheng0815@gmail.com>
2024-02-29 19:52:25 -0500

    feat: setup GitHub OAuth app

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,33 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
-export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
-	//
-	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-	// MY_QUEUE: Queue;
-}
+const ACCESS_TOKEN_URL = 'https://github.com/login/oauth/access_token'
 
 export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
-	},
-};
+  async fetch(
+    request: Request,
+    env: Record<string, string>,
+    ctx: ExecutionContext
+  ): Promise<Response> {
+    const redirectUrl = `${env.APP_URL}/oauth_result.html`
+
+    const code = new URL(request.url).searchParams.get('code')
+    if (!code) {
+      const searchParams = new URLSearchParams({
+        error: 'bad_verification_code',
+        error_description: 'The code passed is incorrect or expired.',
+      }).toString()
+      return Response.redirect(`${redirectUrl}?${searchParams}`, 302)
+    }
+
+    const accessTokenUrl = new URL(ACCESS_TOKEN_URL)
+    accessTokenUrl.search = new URLSearchParams({
+      client_id: env.CLIENT_ID,
+      client_secret: env.CLIENT_SECRET,
+      code,
+    }).toString()
+
+    const githubResponse = await fetch(accessTokenUrl.href, {
+      method: 'POST',
+    })
+    const searchParams = await githubResponse.text()
+    return Response.redirect(`${redirectUrl}?${searchParams}`, 302)
+  },
+}
